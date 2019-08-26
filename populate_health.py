79eceb6e51cdea9bd965464b66ec2ed6f4aa43a0
Xinyu Xiong <2177673x@student.gla.ac.uk>
2019-08-26 18:08:13 +0100

    change in population again

--- populate_health.py
+++ populate_health.py
@@ -23,63 +23,34 @@ def populate():
 
     # making some new users for fun
 
-    doc1 = User.objects.create_user(username='jlennon@beatles.com',email='jlennon@beatles.com',password='glassdoor',user_type=1,first_name='Jlennon',last_name='Beatles')
-    doc2 = User.objects.create_user(username='real@tech.com',email='real@tech.com',password='beautyblender',user_type=1,first_name='Real',last_name='Tech')
-    conduc = User.objects.create_user(username='donald@trump.com',email='donald@trump.com',password='billions',user_type=2,first_name='Donald',last_name='Trump')
-    recep = User.objects.create_user(username='dragrace@vh1.com',email='dragrace@vh1.com',password='kittygirl',user_type=3,first_name='Drag',last_name='Queen')
-    patient1 = User.objects.create_user(username='linda@xiong.com',email='linda@xiong.com',password='xiaotaoqi',user_type=4,first_name='Linda',last_name='Xiong')
-    patient2 = User.objects.create_user(username='adam@burae.com',email='adam@burae.com',password='adamsmith',user_type=4,first_name='Adam',last_name='Burae')
-    patient3 = User.objects.create_user(username='John@sugerman.com',email='John@sugerman.com',password='johnjohnson',user_type=4,first_name='John',last_name='sugerman')
-
-    #making examples for Hospital
-    hos = Hospital.objects.get_or_create(name="Mary Jane Practice",address="21 Beith Street")[0]
-    hos1 = Hospital.objects.create(name="Gardener Practice",address="19 Beith Street")
+    # doc1 = User.objects.create_user(username='jlennon@beatles.com',email='jlennon@beatles.com',password='glassdoor',user_type=1,first_name='Jlennon',last_name='Beatles')
+    # doc2 = User.objects.create_user(username='real@tech.com',email='real@tech.com',password='beautyblender',user_type=1,first_name='Real',last_name='Tech')
+    # conduc = User.objects.create_user(username='donald@trump.com',email='donald@trump.com',password='billions',user_type=2,first_name='Donald',last_name='Trump')
+    # recep = User.objects.create_user(username='dragrace@vh1.com',email='dragrace@vh1.com',password='kittygirl',user_type=3,first_name='Drag',last_name='Queen')
+    # patient1 = User.objects.create_user(username='linda@xiong.com',email='linda@xiong.com',password='xiaotaoqi',user_type=4,first_name='Linda',last_name='Xiong')
+    # patient2 = User.objects.create_user(username='adam@burae.com',email='adam@burae.com',password='adamsmith',user_type=4,first_name='Adam',last_name='Burae')
+    # patient3 = User.objects.create_user(username='John@sugerman.com',email='John@sugerman.com',password='johnjohnson',user_type=4,first_name='John',last_name='sugerman')
+    #
+    # #making examples for Hospital
+    # hos = Hospital.objects.get_or_create(name="Mary Jane Practice",address="21 Beith Street")[0]
+    hos1 = Hospital.objects.get_or_create(name="Gardener Practice",address="19 Beith Street")[0]
 
     # making examples for Department
-    dep1 = Department.objects.create(name='Cardiology',hospital=hos1,tel_no='01234567891')
-    dep2 = Department.objects.create(name='Anaesthetics',hospital=hos1,tel_no='01233567891')
+    dep1 = Department.objects.get_or_create(name='Cardiology',hospital=hos1,tel_no='01234567891')[0]
+    dep2 = Department.objects.get_or_create(name='Anaesthetics',hospital=hos1,tel_no='01233567891')[0]
 
     # making examples for LabTest
-    lab1 = LabTest.objects.get_or_create(patient=User.objects.get(email='linda@xiong.com'),doctor=User.objects.get(email='jlennon@beatles.com'),name='X-ray',conductor=User.objects.get(email='donald@trump.com'))
-    lab2 = LabTest.objects.get_or_create(patient=User.objects.get(email='adam@burae.com'),doctor=User.objects.get(email='jlennon@beatles.com'),name='MRI',conductor=User.objects.get(email='donald@trump.com'))
-
-    # making examples for Disease
-    ibs = Disease.objects.create(name="Irritable bowel syndrome")
-    acne = Disease.objects.create(name="Acne")
-    bv = Disease.objects.create(name="Bacterial Vaginosis")
-    coma = Disease.objects.create(name="Coma")
-    flu = Disease.objects.create(name="flu")
-    diabetes = Disease.objects.create(name="Diabetes")
+    lab1 = LabTest.objects.get_or_create(patient=User.objects.get(email='linda@xiong.com'),doctor=User.objects.get(email='jlennon@beatles.com'),name='X-ray',conductor=User.objects.get(email='donald@trump.com'))[0]
+    lab2 = LabTest.objects.get_or_create(patient=User.objects.get(email='adam@burae.com'),doctor=User.objects.get(email='jlennon@beatles.com'),name='MRI',conductor=User.objects.get(email='donald@trump.com'))[0]
 
     #making examples for schedules
-    s1 = Schedule.objects.create(patient=User.objects.get(email='linda@xiong.com'),date='2019-10-25',time='10:00')
-    s2 = Schedule.objects.create(patient=User.objects.get(email='adam@burae.com'),date='2019-08-22',time='11:00')
-    s3 = Schedule.objects.create(patient=User.objects.get(email='John@sugerman.com'),date='2019-08-22',time='13:00')
-
-    #making examples for medicalSupply
-    syringe = MedicalSupply.objects.create(name='syringe',stock=100)
-    ibruprofen = MedicalSupply.objects.create(name='ibruprofen',stock=1000)
-    iodine = MedicalSupply.objects.create(name='iodine',stock=50)
-    mebeverine = MedicalSupply.objects.create(name='mebeverine',stock=2000)
-
-    #making examples for Order
-    o1 = Order.objects.create(datetime='2019-11-28 08:00+00:00')
-    o2 = Order.objects.create(datetime='2019-01-15 14:00+00:00')
-    o3 = Order.objects.create(datetime='2019-05-18 13:00+00:00')
-
-    #making examples for OrderDetail
-    od1 = Orderdetail.objects.get_or_create(orderid=o1,medicalsupply=syringe,amount=500)
-    od2 = Orderdetail.objects.get_or_create(orderid=o1,medicalsupply=iodine,amount=500)
-    od3 = Orderdetail.objects.get_or_create(orderid=o1,medicalsupply=ibruprofen,amount=500)
+    s1 = Schedule.objects.get_or_create(patient=User.objects.get(email='linda@xiong.com'),date='25/10/2019',time='10:00')[0]
+    s2 = Schedule.objects.get_or_create(patient=User.objects.get(email='adam@burae.com'),date='22/08/2019',time='11:00')[0]
+    s3 = Schedule.objects.get_or_create(patient=User.objects.get(email='John@sugerman.com'),date='22/08/2019',time='13:00')[0]
 
     #making examples for Phr
-    p1 = Phr.objects.get_or_create(patient=User.objects.get(email='linda@xiong.com'),weight='45.8',height='160',doctorid=User.objects.get(email='jlennon@beatles.com'),disease=ibs)
-    p2 = Phr.objects.get_or_create(patient=User.objects.get(email='adam@burae.com'),weight='55',height='170',doctorid=User.objects.get(email='jlennon@beatles.com'),disease=bv)
-
-    # making examples for Ehr
-    ehr1 = Ehr.objects.get_or_create(schedule=s1,medicalsupply=ibruprofen,amount=10)
-    ehr2 = Ehr.objects.get_or_create(schedule=s2,medicalsupply=mebeverine,amount=30)
-    ehr3 = Ehr.objects.get_or_create(schedule=s3,medicalsupply=iodine,amount=2)
+    p1 = Phr.objects.get_or_create(patient=User.objects.get(email='linda@xiong.com'),weight='45.8',height='160',doctorid=User.objects.get(email='jlennon@beatles.com'))[0]
+    p2 = Phr.objects.get_or_create(patient=User.objects.get(email='adam@burae.com'),weight='55',height='170',doctorid=User.objects.get(email='jlennon@beatles.com'))[0]
 
 if __name__=='__main__':
 	print("Starting healthDB population script...")
